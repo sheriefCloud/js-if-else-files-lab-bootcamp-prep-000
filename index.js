7d8663e9899291831f5774eae8e910cb132f2626
Sherief Farid <sherief@farid.cloud>
2019-02-01 21:49:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan"){
   console.log("the name is correct")
+} else {
+  console.log("Expected the name to be Susan")
 }
 
 // Don't worry about this
